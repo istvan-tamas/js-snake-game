c7bb3138d23cb1708da7f9265b3ee5442873957a
Istvan Tamas <istvan.tamas90@gmail.com>
2022-01-22 21:48:11 +0100

    Added some style and testing some JS functions

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-alert("This works!");
+//alert("This works!");
+var c = document.getElementById("gameBoard");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();
