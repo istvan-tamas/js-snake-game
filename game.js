0601ba56d5f5a5657301f8bfbf73a12f77485be0
Istvan Tamas <istvan.tamas90@gmail.com>
2022-02-02 17:05:19 +0100

    Canvas draws pickups in 500 miliseconds after starting the game

--- game.js
+++ game.js
@@ -1,6 +1,24 @@
-//alert("This works!");
-var c = document.getElementById("gameBoard");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+// Event listeners for the controls
+document.querySelector("#start").addEventListener("click", startInterval);
+
+let canvas = document.querySelector("#gameBoard");
+
+function startInterval(){
+    function startGame() {
+        renderGame();
+        setTimeout(function() {
+            startGame();
+        }, 500);
+    }
+    startGame();
+}
+
+function renderGame(){
+    let x = Math.random() * 200;
+    let y = Math.random() * 200;
+    let ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(x, y, 3, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
