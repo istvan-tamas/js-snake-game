4b7c4d94085317e7dfe8252d1b2f6eab022fd3ee
istvan-tamas <istvan.tamas90@gmail.com>
2022-02-10 14:27:06 +0100

    Started implementing powerup generation

--- game.js
+++ game.js
@@ -1,7 +1,9 @@
 // Event listeners for the controls
 document.querySelector("#start").addEventListener("click", startInterval);
 
-//let canvas = document.querySelector("#gameBoard");
+let cnvs = document.querySelector("#gameBoard"); 
+
+
 
 function startInterval(){
     function startGame() {
@@ -16,37 +18,59 @@ function startInterval(){
 function renderGame(){
     let x = Math.random() * 200;
     let y = Math.random() * 200;
-    let ctx = canvas.getContext("2d");
+    let ctx = cnvs.getContext("2d");
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, 2 * Math.PI);
     ctx.stroke();
 }
 
-var canvas = document.getElementById("gameBoard");
-var context = canvas.getContext("2d");
 
+
+
+// random pickup generation
+
+function generatePickUp(){
+    
+    // set random values for positions
+    let xPos = Math.floor(Math.random() * 400 + 1);
+    let yPos = Math.floor(Math.random() * 400 + 1);
+    let ctx = cnvs.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(xPos, yPos, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    console.log(xPos);
+    console.log(yPos);
+}
+
+// testing stuff
+
+generatePickUp();
 
 // drawing a game board with grid
+let ctx = cnvs.getContext("2d");
 
 // Box width
-let bw = 800;
+let bw = 400;
 // Box height
-let bh = 800;
+let bh = 400;
 // Padding
 let p = 5;
 
 function drawBoard(){
-    for (var x = 0; x <= bw; x += 40) {
-        context.moveTo(0.20 + x + p, p);
-        context.lineTo(0.20 + x + p, bh + p);
+    // horizontal lines
+    for (let x = 0; x <= bw; x += 16) {
+        ctx.moveTo(0.20 + x + p, p);
+        ctx.lineTo(0.20 + x + p, bh + p);
     }
 
-    for (var x = 0; x <= bh; x += 40) {
-        context.moveTo(p, 0.20 + x + p);
-        context.lineTo(bw + p, 0.20 + x + p);
+    // vertical lines
+    for (let x = 0; x <= bh; x += 16) {
+        ctx.moveTo(p, 0.20 + x + p);
+        ctx.lineTo(bw + p, 0.20 + x + p);
     }
-    context.strokeStyle = "black";
-    context.stroke();
+    ctx.strokeStyle = "black";
+    ctx.stroke();
 }
 
 drawBoard();
