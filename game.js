524f70a8d5e5761edb57193fba5629c34d54f7b4
istvan-tamas <istvan.tamas90@gmail.com>
2022-02-11 12:44:52 +0100

    Deleted not needed stuff added comments

--- game.js
+++ game.js
@@ -3,8 +3,7 @@ document.querySelector("#start").addEventListener("click", startInterval);
 
 let cnvs = document.querySelector("#gameBoard"); 
 
-
-
+// start the game engine with 3 sec tick
 function startInterval(){
     function startGame() {
         generateRandomPickUp();
@@ -15,25 +14,15 @@ function startInterval(){
     startGame();
 }
 
-function renderGame(){
-    let x = Math.random() * 200;
-    let y = Math.random() * 200;
-    let ctx = cnvs.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(x, y, 3, 0, 2 * Math.PI);
-    ctx.stroke();
-}
-
-
+// random int generator for positioning
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+    let min = Math.ceil(min);
+    let max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
 
 // random pickup generation
-
 function generateRandomPickUp(){
     let ctx = cnvs.getContext("2d");
     // generate a random number between 1-25 (size of the field)
@@ -41,7 +30,7 @@ function generateRandomPickUp(){
     let random_yPos = getRandomInt(1,25);
     ctx.beginPath();
 
-    // to place random powerup add multiples of 16 (1 box is 16x16 pixels)
+    // to place random powerup add multiples of 16 (1 box is 16x16 pixels - 3 pixels to center)
     ctx.arc(16 * random_xPos - 3, 16 * random_yPos - 3, 5, 0, 2 * Math.PI);
     console.log(random_xPos);
     console.log(random_xPos);
@@ -49,7 +38,7 @@ function generateRandomPickUp(){
 
 }
 
-
+// TODO -> randomize first pickup
 function generateFirstPickup(){
     let start_xPos = 13;
     let start_yPos = 13;
